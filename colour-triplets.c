47b3d447c7b81eda524a65b996edccc184fddc04
Markham <quinesquines@gmail.com>
2013-07-20 09:09:32 -0700

    Extern colour triplets to their own c file

---
+++ colour-triplets.c
@@ -0,0 +1,6 @@
+char whiteTriplet[] = {30, 70, 100};
+char redTriplet[] = {255, 0, 0};
+char greenTriplet[] = {0, 255, 0};
+char blueTriplet[] = {0, 0, 255};
+char yellowTriplet[] = {150, 255, 0};
+char orangeTriplet[] = {200, 255, 0};
