7b34ace6d00d7748e3f7de31bed8d89f140cbd87
Markham <quinesquines@gmail.com>
2013-12-08 10:53:36 -0800

    Add arduino-sketch.cpp

---
+++ dowry-box/arduino/arduino-sketch.cpp
@@ -0,0 +1,4 @@
+#include "d:/dev/avr/treasure-hunt-2013/dowry-box/arduino/main.hpp"
+void setup() { Serial.begin(9600); foo(); }
+#include "d:/dev/avr/treasure-hunt-2013/dowry-box/main.cpp"
+void loop() {}
